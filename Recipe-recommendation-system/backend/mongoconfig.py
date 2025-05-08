d9ad69becf5bd03a7617650131b2b2b0df818439
Yuni <68469985+Yuni-Li@users.noreply.github.com>
2025-05-08 22:55:22 +0800

    Delete Recipe-recommendation-system directory

--- Recipe-recommendation-system/backend/mongoconfig.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-URL = 'mongodb://localhost:27017'
-# URL = 'mongodb+srv://Edison:edison123@cluster0.usdxd.mongodb.net/?retryWrites=true&w=majority'
-CLIENT = pymongo.MongoClient(URL)
-DBNAME = 'newdb'
